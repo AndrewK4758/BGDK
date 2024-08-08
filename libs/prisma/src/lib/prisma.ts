e95639d5c906b25fc6eb1160b92079dc6ce3d63c
Andrew <ak475826@gmail.com>
2024-08-08 00:07:40 -0400

    trying secret

--- libs/prisma/src/lib/prisma.ts
+++ libs/prisma/src/lib/prisma.ts
@@ -1,3 +1,6 @@
 import { PrismaClient } from '@prisma/client';
 
-export const prisma = new PrismaClient();
+export const prisma = new PrismaClient({
+  datasourceUrl: process.env['NODE_ENV'] === 'development' ? process.env['DB_URL_SSL'] : process.env['PG_AE_URL'],
+});
+
