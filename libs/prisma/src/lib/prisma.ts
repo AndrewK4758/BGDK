d2e0247de9c052f2237dfc6a49afca05bc86700a
Andrew <ak475826@gmail.com>
2024-08-08 15:28:59 -0400

    added prisma:generate to nx run commands

--- libs/prisma/src/lib/prisma.ts
+++ libs/prisma/src/lib/prisma.ts
@@ -1,7 +1,5 @@
 import { PrismaClient } from '@prisma/client';
 
-process.env['NODE_ENV'] = 'production';
-
 export const prisma = new PrismaClient({
   datasourceUrl: process.env['NODE_ENV'] === 'development' ? process.env['DB_URL_SSL'] : process.env['PG_AE_URL'],
 });
