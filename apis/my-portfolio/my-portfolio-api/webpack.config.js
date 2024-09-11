4f77b7a95fc9f3cb6d8fa80233cfcae25fe82d93
Andrew <ak475826@gmail.com>
2024-09-10 20:41:24 -0400

    first commit for my-portfolio-ui & api

---
+++ apis/my-portfolio/my-portfolio-api/webpack.config.js
@@ -0,0 +1,31 @@
+const { NxAppWebpackPlugin } = require('@nx/webpack/app-plugin');
+const { join } = require('path');
+
+module.exports = {
+  mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
+  output: {
+    path: join(__dirname, '../../../dist/apis/my-portfolio/my-portfolio-api'),
+  },
+  watch: process.env.NODE_ENV === 'development' ? true : false,
+  node: {
+    __dirname: true,
+    __filename: true,
+  },
+  experiments: {
+    topLevelAwait: true,
+  },
+  plugins: [
+    new NxAppWebpackPlugin({
+      target: 'node',
+      compiler: 'tsc',
+      main: './src/main.ts',
+      tsConfig: './tsconfig.app.json',
+      assets: ['./src/assets'],
+      optimization: false,
+      outputHashing: 'none',
+      generatePackageJson: true,
+      watch: true,
+      verbose: true,
+    }),
+  ],
+};
---
+++ apps/my-portfolio/my-portfolio-ui/jest.config.ts
@@ -0,0 +1,11 @@
+/* eslint-disable */
+export default {
+  displayName: 'my-portfolio-ui',
+  preset: '../../../jest.preset.js',
+  transform: {
+    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nx/react/plugins/jest',
+    '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nx/react/babel'] }],
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../../coverage/apps/my-portfolio/my-portfolio-ui',
+};
