3399c59cdcfb104579d6a60481d74b7e128c9b00
Andrew <ak475826@gmail.com>
2024-09-26 01:21:41 -0400

    refactor for all pages and few components, reduced chuck size to 8% of origial

--- apps/portfolio/portfolio/src/pages/crud/crud.tsx
+++ apps/portfolio/portfolio/src/pages/crud/crud.tsx
@@ -1,7 +1,8 @@
 import type { SxProps } from '@mui/material/styles';
 import { Link } from 'react-router-dom';
 import handleScrollIntoView from '../../services/events/handle-scroll-into-view';
-import { lazy } from 'react';
+import { lazy, Suspense } from 'react';
+import { Waiting } from '@bgdk/shared-react-components';
 
 const title = 'Crud App for Data Management';
 const body = `Example of crud app with MUI X- DataGrid, debounced search bar for automatic search without requesting data while typing, uses public ${(<Link to={'https://github.com/lerocha/chinook-database'}>chinook database</Link>)} as the data.`;
@@ -15,14 +16,16 @@ const titleSx: SxProps = {
 const IFrameWithTitle = lazy(() => import('../../components/iframe/iframe-with-title'));
 
 const Crud = () => (
-  <IFrameWithTitle
-    name="crud"
-    title={title}
-    body={body}
-    titleSx={titleSx}
-    bodySx={titleSx}
-    url="http://localhost:5200"
-    scrollFunc={handleScrollIntoView}
-  />
+  <Suspense fallback={<Waiting />}>
+    <IFrameWithTitle
+      name="crud"
+      title={title}
+      body={body}
+      titleSx={titleSx}
+      bodySx={titleSx}
+      url="http://localhost:5200"
+      scrollFunc={handleScrollIntoView}
+    />
+  </Suspense>
 );
 export default Crud;
