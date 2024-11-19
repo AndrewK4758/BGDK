a3a19e8e76261cb40029e68bde4dad28c1064336
Andrew <ak475826@gmail.com>
2024-11-19 11:22:45 -0500

    update for compute engine deploy

--- libs/gen-ai/vertex-ai/src/lib/auth/connection-text-model.ts
+++ libs/gen-ai/vertex-ai/src/lib/auth/connection-text-model.ts
@@ -1,11 +1,4 @@
 import { VertexAI } from '@google-cloud/vertexai';
-import { configDotenv } from 'dotenv';
-import { cwd } from 'process';
-
-configDotenv({ path: `${cwd()}/libs/gen-ai/vertex-ai/env/.env` });
-
-console.log(process.env.VERTEX_CRED_KEY, 'cred key');
-console.log(process.env, 'process env');
 
 // MY STUFF
 export const PROJECT = 'games-424800';
@@ -16,9 +9,6 @@ export const MODEL = 'gemini-1.5-pro-001';
 const vertexAI = new VertexAI({
   project: PROJECT,
   location: LOCATION,
-  googleAuthOptions: {
-    keyFile: process.env.VERTEX_CRED_KEY,
-  },
 });
 
 export default vertexAI;
