f9227466d7e417cce0d40e65f4d5f47cdcbd986b
Andrew <ak475826@gmail.com>
2024-11-22 10:02:16 -0500

    update to pull for compute engine

--- apps/portfolio/portfolio/src/main.tsx
+++ apps/portfolio/portfolio/src/main.tsx
@@ -1,21 +1,13 @@
 // import { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
-import { Waiting } from '@bgdk/shared-react-components';
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import routes from './routes/routes';
 
-const router = createBrowserRouter(routes, {
-  future: {
-    v7_fetcherPersist: true,
-    v7_normalizeFormMethod: true,
-    v7_relativeSplatPath: true,
-    v7_skipActionErrorRevalidation: true,
-  },
-});
+const router = createBrowserRouter(routes);
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   // <StrictMode>
-  <RouterProvider router={router} fallbackElement={<Waiting />} />,
+  <RouterProvider router={router} />,
   // </StrictMode>,
 );
